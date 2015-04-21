ee9acb4277ec6e6ee4882b6bd124df67d174106f
Stefano Mario Chiappa <stefanoc@prismatelecomtesting.com>
2015-04-21 07:41:38 +0200

    Added delegates

---
+++ SynAssember/SynthPanel/DelegateFactoryInterface.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SynthPanels
+{
+	public delegate bool WriteEUProperty(Int32 id, String propertyName, String value);
+
+	public delegate int WriteEUDoubleProperty(int id, int propertyIndex, Double val);
+
+	public delegate int WriteEUIntegerProperty(int id, int propertyIndex, Int32 val);
+
+	public interface DelegateFactoryInterface
+	{
+		WriteEUDoubleProperty getWriteEUDubleProperty();
+	}
+}
---
+++ SynAssember/SynthPanel/SynthDelegateHolder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SynthPanels
+{
+	public struct SynthDelegateHolder
+	{
+		public WriteEUDoubleProperty writeEUDProp;
+		public WriteEUIntegerProperty writeEUIProp;
+	}
+}
