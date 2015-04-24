c215d48cd7557a9435da8850f553e1b2b3e2ac0b
Stefano Mario Chiappa <stefanoc@prismatelecomtesting.com>
2015-04-24 08:19:10 +0200

    Added facilities to control algorithm changes

---
+++ SynAssember/GeneralUtils/Facilities.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeneralUtils
+{
+	public class Facilities
+	{
+		ChangedAlgoruthmInterface changedAlgorithm;
+		public void setCurrentChangeAlgorithm(ChangedAlgoruthmInterface algo)
+		{
+			changedAlgorithm = algo;
+		}
+	}
+}
+
