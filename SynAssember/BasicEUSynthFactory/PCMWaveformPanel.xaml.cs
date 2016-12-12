3fdae34ed658deb042a87b6a1b3566e549a9cf19
Stefano Chiappa <stefanoc@prismatelecomtesting.com>
2016-12-12 09:15:35 +0100

    Adjusted PCMWaveform panel and updated simple image

--- SynAssember/BasicEUSynthFactory/PCMWaveformPanel.xaml.cs
+++ SynAssember/BasicEUSynthFactory/PCMWaveformPanel.xaml.cs
@@ -21,9 +21,44 @@ namespace BasicEUSynthFactory
     /// </summary>
     public partial class PCMWaveformPanel : UserControl
     {
+		private class PitchItem
+		{
+			public String name;
+			public int value;
+			public PitchItem(String name, int value)
+			{
+				this.name = (value > 0 ? "+" : "") + name;
+				this.value = value;
+			}
+
+			public override string ToString()
+			{
+				return name;
+			}
+		}
+
+		PitchItem Zero;
+
+		const int MAX_PITCH_OFFSET = 5;
+
+		void AddAllPitchItems()
+		{
+			int totalRange = MAX_PITCH_OFFSET*2 + 1;
+			for (int i=0;i<totalRange;i++)
+			{
+				int index = i - MAX_PITCH_OFFSET;
+				PitchItem item = new PitchItem(index.ToString(), index);
+				if (item.value == 0)
+					Zero = item;
+				PitchComboBox.Items.Add(item);
+			}
+		}
+
         public PCMWaveformPanel()
         {
             InitializeComponent();
+			AddAllPitchItems();
+			PitchComboBox.SelectedItem = Zero;
         }
 
 		private void BrowseButton_Click(object sender, RoutedEventArgs e)
