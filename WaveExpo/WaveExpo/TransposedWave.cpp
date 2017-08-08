08eef9b763e8093a18db4945074c2824a79f928b
Stefano Chiappa <stefanoc@prismatelecomtesting.com>
2017-08-08 09:52:48 +0200

    Fixed

--- WaveExpo/WaveExpo/TransposedWave.cpp
+++ WaveExpo/WaveExpo/TransposedWave.cpp
@@ -1,7 +1,13 @@
 #include "TransposedWave.h"
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <stdio.h>
+#include <Windows.h>
 
 void TransposedWave::calculateSample(WAVFile * wavFile)
 {
+	/*
+	// Square Wave
 	for (int i = 0; i < 300; i++)
 	{
 		for (int j = 0; j < 200; j++)
@@ -18,4 +24,36 @@ void TransposedWave::calculateSample(WAVFile * wavFile)
 			}
 		}
 	}
+	*/
+
+	double step = 1.0 / 44100.0;
+	double time = 0.0;
+	double sample;
+	int totalSamples = 40000;
+	double freq;
+	int guard = 1000;
+	double hiFreq = 440.0, loFreq = 415.30;
+	double fStep = (hiFreq - loFreq) / ((double)guard * 2);
+	int fStepCounter = 0;
+	for (int i = 0; i < totalSamples; i++)
+	{
+		freq = (i < totalSamples / 2) ? loFreq : hiFreq;
+		/*
+		if (i > (totalSamples / 2 - guard) && i < (totalSamples / 2 + guard))
+		{
+			freq = loFreq + ((double)fStepCounter++) * fStep;
+		}
+		*/
+		char* buffer = new char[1000];
+		memset(buffer, 0, 1000);
+		sprintf_s(buffer, 1000, "i=%d freq=%f\n",i , freq);
+		OutputDebugStringA(buffer);
+
+		sample = sin(2 * M_PI * time * freq);
+		time += step;
+		wavFile->addSample(sample, 0);
+		wavFile->addSample(sample, 1);
+	}
+
 }
+
