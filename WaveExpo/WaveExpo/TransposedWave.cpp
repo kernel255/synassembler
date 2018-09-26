099f485c58da1d9e4893f6e51e523ef933083729
Stefano Chiappa <stefanoc@prismatelecomtesting.com>
2018-09-26 07:13:38 +0200

    Other modifications...

--- WaveExpo/WaveExpo/TransposedWave.cpp
+++ WaveExpo/WaveExpo/TransposedWave.cpp
@@ -6,26 +6,48 @@
 
 void TransposedWave::calculateSample(WAVFile * wavFile)
 {
-	/*
-	// Square Wave
-	for (int i = 0; i < 300; i++)
+	double step = 1.0 / 44100.0;
+	double time = 0.0;
+	double sample;
+	int totalSamples = 400000;
+	double freq;
+	int guard = totalSamples;//11025;// 1000;
+	double startFreq = 220.0, endFreq = 440;
+	double fStep = (endFreq - startFreq) / ((double)guard * 2);
+	int fStepCounter = 0;
+	for (int i = 0; i < totalSamples; i++)
 	{
-		for (int j = 0; j < 200; j++)
+		freq = (i < totalSamples / 2) ? startFreq : endFreq;
+		// Applied only while crossing between the two areas
+		if (i > (totalSamples / 2 - guard) && i < (totalSamples / 2 + guard))
 		{
-			int channel = j % 2;
-			double sample = (i % 2) ? 1.0 : -1.0;
-			if (channel)
-			{
-				wavFile->addSample(sample, channel);
-			}
-			else
-			{
-				wavFile->addSample(sample, channel);
-			}
-		}
-	}
-	*/
 
+			freq = startFreq + (endFreq - startFreq) / 2 * time;
+
+
+			//freq = loFreq + ((double)fStepCounter++) * fStep;
+		}
+		
+
+		// Print freq
+		/*
+		char* buffer = new char[1000];
+		memset(buffer, 0, 1000);
+		sprintf_s(buffer, 1000, "i=%d freq=%f\n",i , freq);
+		OutputDebugStringA(buffer);
+		*/
+
+		sample = sin(2 * M_PI * time * freq);
+		time += step;
+		wavFile->addSample(sample, 0);
+		wavFile->addSample(sample, 1);
+	}
+
+}
+
+/*
+void TransposedWave::calculateSample1(WAVFile * wavFile)
+{
 	double step = 1.0 / 44100.0;
 	double time = 0.0;
 	double sample;
@@ -38,15 +60,14 @@ void TransposedWave::calculateSample(WAVFile * wavFile)
 	for (int i = 0; i < totalSamples; i++)
 	{
 		freq = (i < totalSamples / 2) ? loFreq : hiFreq;
-		/*
-		if (i > (totalSamples / 2 - guard) && i < (totalSamples / 2 + guard))
+		if (i >(totalSamples / 2 - guard) && i < (totalSamples / 2 + guard))
 		{
 			freq = loFreq + ((double)fStepCounter++) * fStep;
 		}
-		*/
+		// Print freq
 		char* buffer = new char[1000];
 		memset(buffer, 0, 1000);
-		sprintf_s(buffer, 1000, "i=%d freq=%f\n",i , freq);
+		sprintf_s(buffer, 1000, "i=%d freq=%f\n", i, freq);
 		OutputDebugStringA(buffer);
 
 		sample = sin(2 * M_PI * time * freq);
@@ -56,4 +77,4 @@ void TransposedWave::calculateSample(WAVFile * wavFile)
 	}
 
 }
-
+*/
